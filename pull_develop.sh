9204d1e452230ba680750b2c722c6179f0ac39e5
frankeg <garrit.franke@ckc.de>
2020-07-13 09:38:38 +0200

    feat: add pull_develop.sh

---
+++ pull_develop.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+
+STASHED_CHANGES=$(git stash | wc -w)
+echo "Changes have been stashed"
+
+git checkout develop
+git pull
+git checkout $CURRENT_BRANCH
+
+if [ $STASHED_CHANGES -gt 5 ] 
+then
+    echo "Changes have been unstashed"
+    git stash pop
+fi
+
+echo "Pull complete"
