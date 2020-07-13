638a0a2a64c91514dbcfb7c50a89e920dde20ed9
garritfra <garritfranke@gmail.com>
2020-07-13 10:44:53 +0200

    docs: add pull_develop docs

--- pull_develop.sh
+++ pull_develop.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env sh
 
+# I often find myself needing to get the latest changes of the develop branch, 
+# to merge it into my current branch. This script lets you pull the develop 
+# branch from any other branch.
+
 CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
 STASHED_CHANGES=$(git stash | wc -w)
