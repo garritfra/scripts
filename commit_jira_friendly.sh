b48d44190ecfc7167ec22943e053b85738cfd2ef
garritfra <garritfranke@gmail.com>
2020-07-21 13:47:27 +0200

    feat: commit_jira_friendly: help

--- commit_jira_friendly.sh
+++ commit_jira_friendly.sh
@@ -3,6 +3,11 @@
 # This script formats a given commit message in a "JIRA-friendly" way. 
 # It prepends the Ticket number to the commit.
 
+if [[ "$1" == "-h" ]]; then
+    echo "Example usage: '$0 fix: important feature'"
+    echo "Output: 'MYPROJ-123 - feat: important feature'"
+    exit 0
+fi
 CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
 TICKET_NUMBER=$(echo $CURRENT_BRANCH | grep -oh "[A-Z]*-[0-9]*" | head -1)
